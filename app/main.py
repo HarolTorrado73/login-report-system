6c273702b88153f1359f109272e3e7087032f424
HaroTorrado73 <ubuntohcmelot@gmail.com>
2026-05-20 16:14:56 -0500

    Refactor application using Flask factory pattern and blueprints

--- app/main.py
+++ app/main.py
@@ -1,11 +1,18 @@
 from flask import Flask, render_template
 
+from app.report import events, generate_report
+
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    report = generate_report(events)
+
+    return render_template(
+        "index.html",
+        report=report
+    )
 
 
 if __name__ == "__main__":
